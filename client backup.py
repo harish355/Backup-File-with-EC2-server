31142b7e7d19502d24c6b8030095795322659036
Harish_kumar_hk <40381981+harish355@users.noreply.github.com>
2020-04-30 11:35:48 +0530

    Update client backup.py

--- client backup.py
+++ client backup.py
@@ -7,8 +7,22 @@ import Tkinter, Tkconstants, tkFileDialog
 def backup():
     # Enter Backup
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.connect(("Ip Address", port))
-    print("Connected to server")
+    c.connect(("ip address", 1234))
+
+    FileName = raw_input("Enter Filename to download from server : ")
+    Data = "Temp"
+
+    while True:
+        c.send(FileName)
+        Data = c.recv(1024)
+        DownloadFile = open(FileName, "wb")
+        while Data:
+            DownloadFile.write(sData)
+            Data = skClient.recv(1024)
+        print("Download Completed")
+        break
+
+    skClient.close()
 
 root = Tk()
 root.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
